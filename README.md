253b3de3ecc063b0f39ae2cfe56b3239a8c91788
moritzloch <loch.moritz@gmail.com>
2023-04-02 17:16:29 +0200

    Small changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Automated Dice Shaker
 
-Project for "Introduction to Microcomputers" by Marco Schweizer & Moritz Loch
+Project for "Introduction to Microcomputers 2" by Marco Schweizer & Moritz Loch
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
+/**
+ * @file main.cpp
+ * @author Marco Schweizer, Moritz Loch
+ * @brief Automated Dice Shaker
+ * @version 1.0
+ * @date 2023-04-02
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup(){
+
+  Serial.begin(9600);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  
 }
